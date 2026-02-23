59c4919d5165460597e2af3b0185847df5af05c6
Lavanya Chopra <lavanyachopra1402@gmail.com>
2026-02-23 23:18:59 +0530

    User Model

--- core/user/user_model.py
+++ core/user/user_model.py
@@ -1,33 +1,46 @@
 class UserModel:
     def __init__(self):
-        self.dominant_lobe = None
+        self.lobe_scores = {
+            "frontal": 0,
+            "temporal": 0,
+            "parietal": 0,
+            "occipital": 0
+            }
         self.preferred_action = None
         self.confusion_level = 0
         self.total_interactions = 0
 
-    def update(self, lobe, action, confidence):
-        self.total_interactions += 1
+        def update(self, lobe, action, confidence):
+            self.total_interactions += 1
+            
+            # Track lobe frequency 
+            if lobe in self.lobe_scores:
+                self.lobe_scores[lobe] += 1
+                
+                # Track action frequency
+                if action not in self.action_scores:
+                    self.action_scores[action] = 0
+                    self.action_scores[action] += 1
+                    # Update confusion
+                    if confidence < 0.5:
+                        self.confusion_level += 1
+                    else:
+                     self.confusion_level = max(0, self.confusion_level - 1)
+                     
+                     def get_dominant_lobe(self):
+                         return max(self.lobe_scores, key=self.lobe_scores.get)
 
-        # Update confusion
-        if confidence < 0.5:
-            self.confusion_level += 1
+def get_preferred_action(self):
+    if not self.action_scores:
+        return None
+    return max(self.action_scores, key=self.action_scores.get)
 
-        # Update dominant lobe
-        if not self.dominant_lobe:
-            self.dominant_lobe = lobe
-
-        # Update preferred action
-        if not self.preferred_action:
-            self.preferred_action = action
-
-    def summary(self):
-        return {
-            "dominant_lobe": self.dominant_lobe,
-            "preferred_action": self.preferred_action,
-            "confusion_level": self.confusion_level,
-            "total_interactions": self.total_interactions
-        }
-
-
+def summary(self):
+    return {
+        "dominant_lobe": self.get_dominant_lobe(),
+        "preferred_action": self.get_preferred_action(),
+        "confusion_level": self.confusion_level,
+        "total_interactions": self.total_interactions
+    }
 # Global user model (single-user system for now)
 user_model = UserModel()
